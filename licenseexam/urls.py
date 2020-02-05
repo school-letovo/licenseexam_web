ce4a7967e7df2d15c5b3af02417dca2e8bc676de
JohnBardoe <jbardoe@yandex.ru>
2020-02-05 15:23:12 +0300

    bugfix

--- licenseexam/urls.py
+++ licenseexam/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from licenseexam import views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
