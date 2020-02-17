ac236c7ce788d7df54d46f4d82f04b94ee0c6643
JohnBardoe <jbardoe@yandex.ru>
2020-02-17 10:13:39 +0300

    Fixed models.py

--- licenseexam/models.py
+++ licenseexam/models.py
@@ -7,6 +7,3 @@ class TestResult(models.Model):
     question_count = models.IntegerField()
     datetime_completed = models.TimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.result_time) + ' ' + str(self.question_count) + ' ' + str(self.user)
