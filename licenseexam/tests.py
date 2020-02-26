26ab327a44c55722c98f58d8d07b8fafbe676abd
JohnBardoe <jbardoe@yandex.ru>
2020-02-26 15:23:56 +0300

    Added tests

---
+++ licenseexam/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase, RequestFactory
+from licenseexam.models import TestResult
+import licenseexam.views
+from django.contrib.auth.models import User
+
+
+class APITestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        User.objects.create(username="unit_test", email='noreply@google.com', password='secure_passw567')
+
+    def testAddSequence(self):
+        request_data = {'user':'unit_test', 'password':'secure_passw567'}
+        request = self.factory.post('api/auth/', request_data, format='multipart')
+        response = licenseexam.views.login(request)
+        self.assertEqual(response.status_code, 200)
+
+        token = response.data["token"]
+        request_data = {'question_count':'12', 'result_time':'123'}
+        request = self.factory.post('api/addresult/', request_data, Authorization=token)
+        response = licenseexam.views.add_new_result(request)
+        self.assertEqual(response.status_code, 200)
