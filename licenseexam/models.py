de83a324f5f5d502451f704f5ad21adc10967572
Yegor Sokolov <egor1sokolov@gmail.com>
2020-02-29 10:17:55 +0300

    added mock test

--- licenseexam/models.py
+++ licenseexam/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
 
 class TestResult(models.Model):
     result_time = models.IntegerField()
     question_count = models.IntegerField()
     datetime_completed = models.TimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __init__(self, time, count):
+        self.result_time = time
+        self.question_count = count
+        self.datetime_completed = datetime.datetime.now()
--- licenseexam/tests.py
+++ licenseexam/tests.py
@@ -20,3 +20,24 @@ class APITestCase(TestCase):
         request = self.factory.post('api/addresult/', request_data, Authorization=token)
         response = licenseexam.views.add_new_result(request)
         self.assertEqual(response.status_code, 200)
+
+class ResultDataTestCase (TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        User.objects.create(username="unit_test", email='noreply@google.com', password='secure_passw567')
+
+    def testTakeResults(self):
+        request_data = {'user': 'unit_test', 'password': 'secure_passw567'}
+        request = self.factory.post('api/auth/', request_data, format='multipart')
+        response = licenseexam.views.login(request)
+
+        token = response.data["token"]
+        request_data = {'question_count': '12', 'result_time': '123'}
+        request = self.factory.post('api/addresult/', request_data, Authorization=token)
+        response = licenseexam.views.add_new_result(request)
+
+        result_data = TestResult.objects.all().filter(user='unit_test')
+        self.assertEqual(len(result_data), 1)
+        for result in result_data:
+            self.assertEqual(result.result_time, 123)
+            self.assertEqual(result.question_count, 12)
