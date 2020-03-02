991bae4d79b09439f269b99541b23fa74470f1c3
JohnBardoe <jbardoe@yandex.ru>
2020-03-02 09:07:44 +0300

    Remove debug logs

--- licenseexam/tests.py
+++ licenseexam/tests.py
@@ -13,7 +13,6 @@ class APITestCase(TestCase):
         request_data = {'username':'unittest', 'password':'securepassw567'}
         request = self.factory.post('api/auth/', request_data, format='multipart')
         response = licenseexam.views.login(request)
-        print(response.data['error'])
         self.assertEqual(response.status_code, 200)
 
         token = response.data["token"]
