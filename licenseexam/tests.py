6fd2656499ab952f857c15cd2b7e14f01ce0709b
JohnBardoe <jbardoe@yandex.ru>
2020-03-02 09:05:28 +0300

    Fix

--- licenseexam/tests.py
+++ licenseexam/tests.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 class APITestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        User.objects.create(username="unit_test", email='noreply@google.com', password='secure_passw567')
+        User.objects.create(username="unittest", password='securepassw567')
 
     def testAddSequence(self):
-        request_data = {'username':'unit_test', 'password':'secure_passw567'}
+        request_data = {'username':'unittest', 'password':'securepassw567'}
         request = self.factory.post('api/auth/', request_data, format='multipart')
         response = licenseexam.views.login(request)
         print(response.data['error'])
