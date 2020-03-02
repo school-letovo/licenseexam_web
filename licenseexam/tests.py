43aa914827222d30c5ca853056ca3fa3cb3687d5
JohnBardoe <jbardoe@yandex.ru>
2020-03-02 09:43:10 +0300

    Fix

--- licenseexam/tests.py
+++ licenseexam/tests.py
@@ -17,7 +17,7 @@ class APITestCase(TestCase):
 
         token = response.data["token"]
         request_data = {'question_count':'12', 'result_time':'123'}
-        request = self.factory.post('api/addresult/', request_data, format='multipart', Authorization='Token ' + token)
+        request = self.factory.post('api/addresult/', request_data, format='multipart', HTTP_AUTHORIZATION='Token ' + token)
         response = licenseexam.views.add_new_result(request)
         print(request.headers, '  ', response.data)
         self.assertEqual(response.status_code, 200)
